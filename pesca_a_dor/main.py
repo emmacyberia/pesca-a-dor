38e7ea7e999ace2272fc6d2ffd53c043dc0e1ada
Felipe Vaz Peres <felipe.vzps@gmail.com>
2025-02-28 18:49:01 -0300

    refactor

--- pesca_a_dor/main.py
+++ pesca_a_dor/main.py
@@ -1,17 +1,4 @@
+from core.actions import main
 
-import argparse
-from core.pesca_a_dor import main_pesca_a_dor
-from core.money_maker import main_money_maker
-
-def main():
-    parser = argparse.ArgumentParser(description="pesca-a-dor: fishing bot for PokeXGames")
-    parser.add_argument('-m', '--mode', type=int, choices=[1, 2], required=True, help='Select mode: 1 for pesca-a-dor, 2 for money-maker')
-    args = parser.parse_args()
-    
-    if args.mode == 1:
-        main_pesca_a_dor()
-    elif args.mode == 2:
-        main_money_maker()
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
